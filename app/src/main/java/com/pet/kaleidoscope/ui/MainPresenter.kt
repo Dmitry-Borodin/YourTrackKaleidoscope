ef3fa8e8c39db5bf681744c30b6a745ad7a4e02a
Dmitry Borodin <mail@DmitryBorodin.com>
2019-02-28 23:03:58 +0100

    soma old changes

--- app/src/main/java/com/pet/kaleidoscope/ui/MainPresenter.kt
+++ app/src/main/java/com/pet/kaleidoscope/ui/MainPresenter.kt
@@ -3,3 +3,17 @@ package com.pet.kaleidoscope.ui
 /**
  * @author Dmitry Borodin on 2/22/19.
  */
+class MainPresenter(val view: MainView) {
+
+    fun start() {
+
+    }
+
+    fun stop() {
+
+    }
+
+    fun fetchCurrentState() {
+
+    }
+}
