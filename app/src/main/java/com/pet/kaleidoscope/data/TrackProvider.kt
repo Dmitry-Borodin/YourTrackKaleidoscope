ac86f8ba7e2fffc7ed0abe775e02a36a1e346967
Dmitry Borodin <mail@DmitryBorodin.com>
2019-03-10 16:50:05 +0100

    flickr4java added

--- app/src/main/java/com/pet/kaleidoscope/data/TrackProvider.kt
+++ app/src/main/java/com/pet/kaleidoscope/data/TrackProvider.kt
@@ -1,8 +1,21 @@
 package com.pet.kaleidoscope.data
 
+import com.flickr4java.flickr.REST
+import com.flickr4java.flickr.Flickr
+
+
+
 /**
  * @author Dmitry Borodin on 2/22/19.
  */
 object TrackProvider {
     fun getTracksChannel() = Unit
+
+    fun getFlickrResults() {
+        val apiKey = "YOUR_API_KEY"
+        val sharedSecret = "YOUR_SHARED_SECRET"
+        val flickr = Flickr(apiKey, sharedSecret, REST())
+        val testInterface = flickr.testInterface
+        val results = testInterface.echo(mapOf())
+    }
 }
