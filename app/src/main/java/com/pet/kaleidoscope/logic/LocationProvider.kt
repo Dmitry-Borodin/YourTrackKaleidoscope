a694101a6ba7e41920f32068069a4d381e832ed7
Dmitry Borodin <mail@DmitryBorodin.com>
2019-03-16 23:40:18 +0100

    location service implemented, small work around

--- app/src/main/java/com/pet/kaleidoscope/logic/LocationProvider.kt
+++ app/src/main/java/com/pet/kaleidoscope/logic/LocationProvider.kt
@@ -1,8 +1,41 @@
 package com.pet.kaleidoscope.logic
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.location.Location
+import android.os.IBinder
+import com.pet.kaleidoscope.service.LocationService
+import kotlinx.coroutines.channels.Channel
+
 /**
  * @author Dmitry Borodin on 3/14/19.
  */
-class LocationProvider {
+class LocationProvider(private val appContext: Context) {
 
+    private val serviceConnection = object ::ServiceConnection() {
+        override fun onServiceConnected(className: android.content.ComponentName, service: android.os.IBinder) {
+            importService = (service as ApplicationMigrationService.ApplicationMigrationBinder).getService()
+            importService.setImportStateHandler(importStateHandler)
+
+            val state = importService.getState()
+            importStateHandler.obtainMessage(state.state, state.progress).sendToTarget()
+        }
+
+        override fun onServiceDisconnected(name: android.content.ComponentName) {
+            importService.setImportStateHandler(null)
+        }
+    }
+
+    var isTrackingInProgress = false
+    var resultChannel: Channel<Location> = Channel()
+
+    fun startLocationTracking(): Channel<Location> {
+        appContext.bindService(Intent(appContext, LocationService.class), )
+    }
+
+    fun stopLocationTracking() {
+        appContext.unbindService()
+    }
 }
---
+++ app/src/main/java/com/pet/kaleidoscope/service/LocationService.kt
@@ -0,0 +1,64 @@
+package com.pet.kaleidoscope.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.pet.kaleidoscope.R
+
+/**
+ * @author Dmitry Borodin on 3/16/19.
+ */
+class LocationService : Service() {
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when (intent?.action) {
+            START_TRACKING -> {
+                startForeground()
+            }
+            STOP_TRACKING -> {
+                stopForeground()
+                stopSelf()
+            }
+        }
+        return START_NOT_STICKY //TODO support sticky
+    }
+
+    override fun onBind(intent: Intent?): IBinder? { return null
+    }
+
+    private fun startForeground() {
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(this, CHANNEL_SERVICE_NOTOFICATION)
+            .setContentText(getString(R.string.notification_tracking_content))
+            .setSmallIcon(R.drawable.ic_launcher)
+            .build()
+        startForeground(1, notification)
+    }
+
+    private fun stopForeground() {
+        stopForeground(true)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_tracking_name)
+            val descriptionText = getString(R.string.notification_channel_tracking_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(CHANNEL_SERVICE_NOTOFICATION, name, importance)
+            channel.description = descriptionText
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+    }
+
+    companion object {
+        const val START_TRACKING = "START_TRACKING"
+        const val STOP_TRACKING = "STOP_TRACKING"
+        const val CHANNEL_SERVICE_NOTOFICATION = "CHANNEL_SERVICE_NOTOFICATION"
+    }
+}
